313a7bb6f5fe0ca7c9c8c6582a5bf9b7dbe2a93e
simonalveteg <simon.alveteg@gmail.com>
2024-12-13 14:15:07 +0100

    Lab3: remove commentsgit push!

--- labs/lab3/preflow_barrier_cp.c
+++ labs/lab3/preflow_barrier_cp.c
@@ -1,32 +1,3 @@
-/* This is an implementation of the preflow-push algorithm, by
- * Goldberg and Tarjan, for the 2021 EDAN26 Multicore programming labs.
- *
- * It is intended to be as simple as possible to understand and is
- * not optimized in any way.
- *
- * You should NOT read everything for this course.
- *
- * Focus on what is most similar to the pseudo code, i.e., the functions
- * preflow, push, and relabel.
- *
- * Some things about C are explained which are useful for everyone  
- * for lab 3, and things you most likely want to skip have a warning 
- * saying it is only for the curious or really curious. 
- * That can safely be ignored since it is not part of this course.
- *
- * Compile and run with: make
- *
- * Enable prints by changing from 1 to 0 at PRINT below.
- *
- * Feel free to ask any questions about it on Discord 
- * at #lab0-preflow-push
- *
- * A variable or function declared with static is only visible from
- * within its file so it is a good practice to use in order to avoid
- * conflicts for names which need not be visible from other files.
- *
- */
- 
 #include <assert.h>
 #include <ctype.h>
 #include <stdarg.h>
@@ -37,14 +8,6 @@
 
 #define PRINT		0	/* enable/disable prints. */
 
-/* the funny do-while next clearly performs one iteration of the loop.
- * if you are really curious about why there is a loop, please check
- * the course book about the C preprocessor where it is explained. it
- * is to avoid bugs and/or syntax errors in case you use the pr in an
- * if-statement without { }.
- *
- */
-
 #if PRINT
 #define pr(...)		do { fprintf(stderr, __VA_ARGS__); } while (0)
 #else
@@ -53,15 +16,6 @@
 
 #define MIN(a,b)	(((a)<=(b))?(a):(b))
 #define MAX(a,b)	(((a)<=(b))?(b):(a))
-
-/* introduce names for some structs. a struct is like a class, except
- * it cannot be extended and has no member methods, and everything is
- * public.
- *
- * using typedef like this means we can avoid writing 'struct' in 
- * every declaration. no new type is introduded and only a shorter name.
- *
- */
 
 typedef struct graph_t	graph_t;
 typedef struct node_t	node_t;
@@ -126,88 +80,18 @@ struct args_t {
 	int stop;
 };
 
-/* a remark about C arrays. the phrase above 'array of n nodes' is using
- * the word 'array' in a general sense for any language. in C an array
- * (i.e., the technical term array in ISO C) is declared as: int x[10],
- * i.e., with [size] but for convenience most people refer to the data
- * in memory as an array here despite the graph_t's v and e members 
- * are not strictly arrays. they are pointers. once we have allocated
- * memory for the data in the ''array'' for the pointer, the syntax of
- * using an array or pointer is the same so we can refer to a node with
- *
- * 			g->v[i]
- *
- * where the -> is identical to Java's . in this expression.
- * 
- * in summary: just use the v and e as arrays.
- * 
- * a difference between C and Java is that in Java you can really not
- * have an array of nodes as we do. instead you need to have an array
- * of node references. in C we can have both arrays and local variables
- * with structs that are not allocated as with Java's new but instead
- * as any basic type such as int.
- * 
- */
-
 static char* progname;
 
 #if PRINT
 
 static int id(graph_t* g, node_t* v)
 {
-	/* return the node index for v.
-	 *
-	 * the rest is only for the curious.
-	 *
-	 * we convert a node pointer to its index by subtracting
-	 * v and the array (which is a pointer) with all nodes.
-	 *
-	 * if p and q are pointers to elements of the same array,
-	 * then p - q is the number of elements between p and q.
-	 *
-	 * we can of course also use q - p which is -(p - q)
-	 *
-	 * subtracting like this is only valid for pointers to the
-	 * same array.
-	 *
-	 * what happens is a subtract instruction followed by a
-	 * divide by the size of the array element.
-	 *
-	 */
-
 	return v - g->v;
 }
 #endif
 
 void error(const char* fmt, ...)
 {
-	/* print error message and exit. 
-	 *
-	 * it can be used as printf with formatting commands such as:
-	 *
-	 *	error("height is negative %d", v->h);
-	 *
-	 * the rest is only for the really curious. the va_list
-	 * represents a compiler-specific type to handle an unknown
-	 * number of arguments for this error function so that they
-	 * can be passed to the vsprintf function that prints the
-	 * error message to buf which is then printed to stderr.
-	 *
-	 * the compiler needs to keep track of which parameters are
-	 * passed in integer registers, floating point registers, and
-	 * which are instead written to the stack.
-	 *
-	 * avoid ... in performance critical code since it makes 
-	 * life for optimizing compilers much more difficult. but in
-	 * in error functions, they obviously are fine (unless we are
-	 * sufficiently paranoid and don't want to risk an error 
-	 * condition escalate and crash a car or nuclear reactor 		 
-	 * instead of doing an even safer shutdown (corrupted memory
-	 * can cause even more damage if we trust the stack is in good
-	 * shape)).
-	 *
-	 */
-
 	va_list		ap;
 	char		buf[BUFSIZ];
 
@@ -226,20 +110,6 @@ static int next_int()
         int     x;
         int     c;
 
-	/* this is like Java's nextInt to get the next integer.
-	 *
-	 * we read the next integer one digit at a time which is
-	 * simpler and faster than using the normal function
-	 * fscanf that needs to do more work.
-	 *
-	 * we get the value of a digit character by subtracting '0'
-	 * so the character '4' gives '4' - '0' == 4
-	 *
-	 * it works like this: say the next input is 124
-	 * x is first 0, then 1, then 10 + 2, and then 120 + 4.
-	 *
-	 */
-
 	x = 0;
         while (isdigit(c = getchar()))
                 x = 10 * x + c - '0';
@@ -250,19 +120,6 @@ static int next_int()
 static void* xmalloc(size_t s)
 {
 	void*		p;
-
-	/* allocate s bytes from the heap and check that there was
-	 * memory for our request.
-	 *
-	 * memory from malloc contains garbage except at the beginning
-	 * of the program execution when it contains zeroes for 
-	 * security reasons so that no program should read data written
-	 * by a different program and user.
-	 *
-	 * size_t is an unsigned integer type (printed with %zu and
-	 * not %d as for int).
-	 *
-	 */
 
 	p = malloc(s);
 
@@ -281,30 +138,12 @@ static void* xcalloc(size_t n, size_t s)
 	/* memset sets everything (in this case) to 0. */
 	memset(p, 0, n * s);
 
-	/* for the curious: so memset is equivalent to a simple
-	 * loop but a call to memset needs less memory, and also
- 	 * most computers have special instructions to zero cache 
-	 * blocks which usually are used by memset since it normally
-	 * is written in assembler code. note that good compilers 
-	 * decide themselves whether to use memset or a for-loop
-	 * so it often does not matter. for small amounts of memory
-	 * such as a few bytes, good compilers will just use a 
-	 * sequence of store instructions and no call or loop at all.
-	 *
-	 */
-
 	return p;
 }
 
 static void add_edge(node_t* u, edge_t* e)
 {
 	list_t*		p;
-
-	/* allocate memory for a list link and put it first
-	 * in the adjacency list of u.
-	 *
-	 */
-
 	p = xmalloc(sizeof(list_t));
 	p->edge = e;
 	p->next = u->edge;
@@ -313,11 +152,6 @@ static void add_edge(node_t* u, edge_t* e)
 
 static void connect(node_t* u, node_t* v, int c, edge_t* e)
 {
-	/* connect two nodes by putting a shared (same object)
-	 * in their adjacency lists.
-	 *
-	 */
-
 	e->u = u;
 	e->v = v;
 	e->c = c;
@@ -372,7 +206,6 @@ static void push(graph_t* g, node_t* u, node_t* v, edge_t* e, int d)
 {
 	pr("push from %d to %d: ", id(g, u), id(g, v));
 	pr("f = %d, c = %d, so ", e->f, e->c);
-	
 	pr("pushing %d\n", abs(d));
 
 
@@ -417,14 +250,6 @@ cmd_list_t* get_command(graph_t* g, node_t* u) // Previously dispatch
 	cmd_list_t* c_list = calloc(1, sizeof(cmd_list_t));
 	pr("selected u = %d with ", id(g, u));
 	pr("h = %d and e = %d\n", u->h, u->e);
-
-	/* if we can push we must push and only if we could
-		* not push anything, we are allowed to relabel.
-		*
-		* we can push to multiple nodes if we wish but
-		* here we just push once for simplicity.
-		*
-		*/
 
 	if (u->e == 0){
 		free(c_list);
@@ -583,19 +408,12 @@ int preflow(graph_t* g, int thread_amount)
 
 	p = s->edge;
 
-	/* start by pushing as much as possible (limited by
-	 * the edge capacity) from the source to its neighbors.
-	 *
-	 */
-
 	while (p != NULL) {
 		e = p->edge;
 		p = p->next;
 
 		push(g, s, other(s, e), e, e->c);
 	}
-
-	/* then loop until only s and/or t have excess preflow. */
 
 	g->done = 0;
 
@@ -651,7 +469,7 @@ static void free_graph(graph_t* g)
 	free(g);
 }
 
-int main(int argc, char* argv[])
+int smain(int argc, char* argv[])
 {
 	FILE*		in;	/* input file set to stdin	*/
 	graph_t*	g;	/* undirected graph. 		*/
